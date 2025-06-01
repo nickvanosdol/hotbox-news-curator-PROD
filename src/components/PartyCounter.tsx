91cb9fd3ce7f920affbdebe4991c63a81c990980
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2025-05-31 22:13:06 -0600

    Update PartyCounter.tsx

--- src/components/PartyCounter.tsx
+++ src/components/PartyCounter.tsx
@@ -1,6 +1,6 @@
 export default function PartyCounter() {
   return (
-    <div className="fixed group bottom-12 right-5 lg:-right-33 z-50 bg-neutral-50 text-gray-600 p-3 rounded-md rounded-bl-md shadow-sm hover:bg-[var(--color-primary-100)] transition-all lg:hover:-translate-x-36 lg:focus:-translate-x-36">
+    <div className="fixed group bottom-12 right-0 lg:-right-33 z-50 bg-neutral-50 text-gray-600 p-3 lg:rounded-md rounded-tl-md rounded-bl-md shadow-sm hover:bg-[var(--color-primary-100)] transition-all lg:hover:-translate-x-36 focus:-translate-x-36">
       🔥 <strong>27</strong>{' '}
       <span className="lg:opacity-0 lg:group-hover:opacity-100">
         currently hotboxing
