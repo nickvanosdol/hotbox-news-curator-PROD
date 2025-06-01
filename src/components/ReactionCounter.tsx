99857e71dc569f4c003826336764fb32ee5f60be
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2025-05-31 23:30:23 -0600

    subtlety

--- src/components/ReactionCounter.tsx
+++ src/components/ReactionCounter.tsx
@@ -12,7 +12,7 @@ export default function ReactionCounter() {
   return (
     <button
       onClick={handleClick}
-      className="py-1 px-3 mt-2 shadow-sm rounded-lg bg-[var(--color-primary-50)] hover:shadow-none cursor-pointer focus:shadow-none">
+      className="py-1 px-3 mt-2 shadow-sm rounded-lg bg-[var(--color-primary-50)] transition-all hover:grayscale-0 active:grayscale-0 grayscale-50 hover:shadow-none cursor-pointer focus:shadow-none">
       🔥 {count}
     </button>
   );
