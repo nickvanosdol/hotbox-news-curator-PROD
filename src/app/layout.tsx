009b4f747adfd6b0dfa933aa0a4cbef2079a5465
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2025-07-24 14:45:04 -0600

    newspaper styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${chewy.variable} ${outfit.variable} antialiased bg-white`}>
+        className={`${chewy.variable} ${outfit.variable} antialiased bg-[rgba(245, 245, 245, 1)]`}>
+        <div
+          className="pointer-events-none absolute bottom-0 top-0 left-0 right-0 opacity-30 -z-100"
+          style={{
+            backgroundImage: 'url("/svg/noise.svg")',
+            backgroundRepeat: 'repeat',
+            backgroundSize: '182px 182px',
+          }}
+          aria-hidden="true"
+        />
         {children}
         {/* <div
           className="pointer-events-none fixed inset-0 -z-10 opacity-35"
